f18e02c3d8fb117c4a560c7cc425e605683a2b72
Freddy Martínez <fhmartinezq@gmail.com>
2024-03-03 20:54:45 +0100

    refactor: remove duplicated request

--- UdemyNodeTDD/__tests__/user-register.spec.ts
+++ UdemyNodeTDD/__tests__/user-register.spec.ts
@@ -19,45 +19,34 @@ beforeEach(() => {
 });
 
 describe("UserRegister", () => {
+  const postReqValidUser = () => request(app).post(SIGNUP_URI).send(user);
+
   it("should return 200 Ok when signup request is valid", (done) => {
-    request(app)
-      .post(SIGNUP_URI)
-      .send(user)
-      .expect(200, done);
+    postReqValidUser().expect(200, done);
   });
 
   it("should return success message when signup request is valid", (done) => {
-    request(app)
-      .post(SIGNUP_URI)
-      .send(user)
-      .then((response) => {
-        expect(response.body).toEqual({ message: "User registered" });
-        done();
-      });
+    postReqValidUser().then((response) => {
+      expect(response.body).toEqual({ message: "User registered" });
+      done();
+    });
   });
 
   it("should return 400 Bad Request when username is missing", (done) => {
     const incompleteUser: Record<string, string> = { ...user };
     delete incompleteUser.username;
-    request(app)
-      .post(SIGNUP_URI)
-      .send(incompleteUser)
-      .expect(400, done);
+    request(app).post(SIGNUP_URI).send(incompleteUser).expect(400, done);
   });
-  
+
   it("should return 400 Bad Request when email is missing", async () => {
     const incompleteUser: Record<string, string> = { ...user };
     delete incompleteUser.email;
-    const response = await request(app)
-      .post(SIGNUP_URI)
-      .send(incompleteUser);
+    const response = await request(app).post(SIGNUP_URI).send(incompleteUser);
     expect(response.status).toBe(400);
   });
 
   it("should save the user to the database", (done) => {
-    request(app)
-      .post(SIGNUP_URI)
-      .send(user)
+    postReqValidUser()
       .then(() => {
         return User.findAll({ where: { email: user.email } });
       })
@@ -67,8 +56,8 @@ describe("UserRegister", () => {
       });
   });
 
-  it("should save the user's password as a hash", async() => {
-    const response = await request(app).post(SIGNUP_URI).send(user);
+  it("should save the user's password as a hash", async () => {
+    const response = await postReqValidUser();
     expect(response.status).toBe(200);
     const users = await User.findAll({ where: { email: user.email } });
     expect(users.length).toBe(1);
