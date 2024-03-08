118c21b643f3f99cce42dd925007df2abcc2679a
Freddy Martínez <fhmartinezq@gmail.com>
2024-03-08 21:11:53 +0100

    refactor: move validation into handlers

--- UdemyNodeTDD/src/routes/userRouter.ts
+++ UdemyNodeTDD/src/routes/userRouter.ts
@@ -1,29 +1,49 @@
-import { Router } from "express";
+import { Request, RequestHandler, Router } from "express";
 import { SIGNUP_URI } from "../../util/constants";
 import { createUser } from "../bll/user";
 
 const router = Router();
 
-router.post(SIGNUP_URI, (req, res, next) => {
-  const { username, email, password } = req.body;
+interface CustomRequest extends Request {
+  validationErrors?: Record<string, string>;
+}
 
-  const validationErrors: Record<string, string> = {};
+const validateUser: RequestHandler = (req: CustomRequest, _, next) => {
+  const { username } = req.body;
+
   if (!username ) {
-    validationErrors.username = "Username is required";
-  }
-  if (!email) {
-    validationErrors.email = "Email is required";
-  }
-  if (!password) {
-    validationErrors.password = "Password is required";
-  }
-
-  if (Object.keys(validationErrors).length > 0) {
-    return res.status(400).send({ validationErrors });
+    req.validationErrors = { username: "Username is required"};
   }
 
   next();
-} , async (req, res) => {
+};
+
+
+const validateEmail: RequestHandler = (req: CustomRequest, _, next) => {
+  const { email } = req.body;
+
+  if (!email) {
+    req.validationErrors = { ...req.validationErrors, email: "Email is required" };
+  }
+
+  next();
+};
+
+const validatePassword: RequestHandler = (req: CustomRequest, _, next) => {
+  const { password } = req.body;
+
+  if (!password) {
+    req.validationErrors = { ...req.validationErrors, password: "Password is required" };
+  }
+
+  next();
+};
+
+router.post(SIGNUP_URI, validateUser, validateEmail, validatePassword, async (req: CustomRequest, res) => {
+  if (req.validationErrors) {
+    return res.status(400).send({ validationErrors: req.validationErrors });
+  }
+
   const [status, message] = await createUser(req.body);
   res.status(status).send({ message });
 });
