22655577f271a241b6354cf94cd57b4a401827bd
Freddy Martínez <fhmartinezq@gmail.com>
2024-03-09 20:07:51 +0100

    refactor: use express-validator to check incomming data

--- UdemyNodeTDD/src/routes/userRouter.ts
+++ UdemyNodeTDD/src/routes/userRouter.ts
@@ -1,51 +1,31 @@
-import { Request, RequestHandler, Router } from "express";
+import { Router } from "express";
 import { SIGNUP_URI } from "../../util/constants";
 import { createUser } from "../bll/user";
+import { check, validationResult } from "express-validator";
 
 const router = Router();
 
-interface CustomRequest extends Request {
-  validationErrors?: Record<string, string>;
-}
+router.post(
+  SIGNUP_URI,
+  check("username").notEmpty().withMessage("Username is required"),
+  check("email").notEmpty().withMessage("Email is required"),
+  check("password").notEmpty().withMessage("Password is required"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+      const validationErrors: Record<string, string> = {};
+      errors.array().forEach((error) => {
+        if (error.type === "field") {
+          validationErrors[error.path] = error.msg;
+        }
+      });
+      return res.status(400).send({validationErrors});
+    }
 
-const validateUser: RequestHandler = (req: CustomRequest, _, next) => {
-  const { username } = req.body;
-
-  if (!username ) {
-    req.validationErrors = { username: "Username is required"};
+    const [status, message] = await createUser(req.body);
+    res.status(status).send({ message });
   }
-
-  next();
-};
-
-
-const validateEmail: RequestHandler = (req: CustomRequest, _, next) => {
-  const { email } = req.body;
-
-  if (!email) {
-    req.validationErrors = { ...req.validationErrors, email: "Email is required" };
-  }
-
-  next();
-};
-
-const validatePassword: RequestHandler = (req: CustomRequest, _, next) => {
-  const { password } = req.body;
-
-  if (!password) {
-    req.validationErrors = { ...req.validationErrors, password: "Password is required" };
-  }
-
-  next();
-};
-
-router.post(SIGNUP_URI, validateUser, validateEmail, validatePassword, async (req: CustomRequest, res) => {
-  if (req.validationErrors) {
-    return res.status(400).send({ validationErrors: req.validationErrors });
-  }
-
-  const [status, message] = await createUser(req.body);
-  res.status(status).send({ message });
-});
+);
 
 export { router as userRouter };
